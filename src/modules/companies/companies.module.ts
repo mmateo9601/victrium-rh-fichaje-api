61811e0df63ec1b45b4d36a1923ce50f6e48b95e
mmateo9601 <mmateo9601@users.noreply.github.com>
2026-08-24 14:33:07 +0200

    Fix company module repository registrations

--- src/modules/companies/companies.module.ts
+++ src/modules/companies/companies.module.ts
@@ -4,13 +4,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TenantScopeService } from '../../common/tenant/tenant-scope.service';
 import { CalendarEntity } from '../../database/entities/calendar.entity';
 import { CompanyEntity } from '../../database/entities/company.entity';
+import { EmployeeEntity } from '../../database/entities/employee.entity';
+import { PlanningPeriodEntity } from '../../database/entities/planning-period.entity';
+import { UserEntity } from '../../database/entities/user.entity';
+import { WorkLocationEntity } from '../../database/entities/work-location.entity';
 import { UsersModule } from '../users/users.module';
 import { WorkLocationsModule } from '../work-locations/work-locations.module';
 import { CompaniesController } from './companies.controller';
 import { CompaniesService } from './companies.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CompanyEntity, CalendarEntity]), UsersModule, WorkLocationsModule],
+  imports: [
+    TypeOrmModule.forFeature([CompanyEntity, CalendarEntity, EmployeeEntity, UserEntity, WorkLocationEntity, PlanningPeriodEntity]),
+    UsersModule,
+    WorkLocationsModule
+  ],
   controllers: [CompaniesController],
   providers: [CompaniesService, TenantScopeService],
   exports: [CompaniesService]
